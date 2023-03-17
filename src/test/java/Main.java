1f1d321dcabf9ab480c561c79cb0487a463ab655
Ruslan Munasipov <ruslanmunasipov23@gmail.com>
2023-03-17 17:59:38 +0300

    added: new func called app

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(sum(10, 20));
+        System.out.println(app(10, 20));
     }
 
-    public static int sum(int c, int d) {
-        int sum = c + d;
-        return sum;
+    public static int app(int c, int d) {
+        int app = c + d;
+        return app;
     }
 }
