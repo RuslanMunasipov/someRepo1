7eb51c05bb1dfc3af3c85a337a824395d53ef932
Ruslan Munasipov <ruslanmunasipov23@gmail.com>
2023-03-17 17:55:45 +0300

    added: new func called sum

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(sum(15, 20));
+    }
+
+    public static int sum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
 }
