37340c6ef3450c9ee56e4302f7908409980239e9
Ruslan Munasipov <ruslanmunasipov23@gmail.com>
2023-03-17 17:59:06 +0300

    added: new func called del

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(sum(15, 20));
+        System.out.println(del(15, 20));
     }
 
-    public static int sum(int a, int b) {
-        int sum = a + b;
-        return sum;
+    public static int del(int a, int b) {
+        int del = a + b;
+        return del;
     }
 }
